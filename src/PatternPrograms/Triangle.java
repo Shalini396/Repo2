8d4a374358ac212ebf401c7b70b8963e2a89d45c
Shalini396 <85531588+Shalini396@users.noreply.github.com>
2023-06-20 11:26:53 +0530

    First Commit

--- src/PatternPrograms/Triangle.java
+++ src/PatternPrograms/Triangle.java
@@ -1,7 +1,7 @@
 package PatternPrograms;
 
 public class Triangle {
-	void printPattern(int n) {
+	void printPatternL(int n) {
 		int i,j;
 		for(i=1;i<=n;i++) {
 			for(j=1;j<=i;j++) {
@@ -11,11 +11,25 @@ public class Triangle {
 		}
 		
 	}
+	void printPatternR(int n) {
+		int i,j,k;
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(k=1;k<=i;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Triangle obj = new Triangle();
-		obj.printPattern(10);
+		Triangle obj2 = new Triangle();
+		obj.printPatternL(10);
+		obj2.printPatternR(10);
 		
 
 	}
